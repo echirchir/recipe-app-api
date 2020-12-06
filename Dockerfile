63ea5b84729bc6c1bab104d2c38e3f6803835623
Elisha <elisha.java@gmail.com>
2020-12-06 17:51:33 +0300

    Adds user creation api

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
-FROM python:3.6-alpine
+FROM python:3.6.8
 
 ENV PYTHONUNBUFFERED  1
 
 COPY ./requirements.txt /requirements.txt 
-RUN apk add --update --no-cache postgresql-client
-RUN apk add postgresql-dev=9.6.10-r0 --repository=http://dl-cdn.alpinelinux.org/alpine/v3.5/main gcc
-RUN apk add postgresql-libs libpq --no-cache
+RUN apt-get update && apt-get install -y gettext libgettextpo-dev graphviz default-jdk
 RUN pip install -r /requirements.txt
-RUN apk del .tmp-build-deps
 
 RUN mkdir /app 
 WORKDIR /app 
 COPY ./app /app 
 
-RUN adduser -D user
+RUN adduser user
 USER user
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,15 +10,15 @@ class CommandTests(TestCase):
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
         
-        with path('django.db.utils.ConnectionHandler.__getitem__') as gi:
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.return_value = True 
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
             
-    @path('time.sleep', return_value=True)
+    @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
-        with patch('django.db.utils.ConnectionHandler.__getitem___') as gi:
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 6)
